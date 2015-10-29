969861b529ae151e226037e09e39aab081f14a37
azaczek <azaczek@gbox.pl>
2015-10-29 12:55:34 +0100

    refactoring

--- testy/logowanieTest.php
+++ testy/logowanieTest.php
@@ -1,25 +1,20 @@
 <?php
-class BananaScrumTest extends PHPUnit_Extensions_Selenium2TestCase{
-	
-	protected function setUp()
-	{
-		$this->setBrowser('firefox');
-		$this->setBrowserUrl('https://szkolenia.bananascrum.com');		
-	
+class BananaScrumTest extends PHPUnit_Extensions_Selenium2TestCase {
+	protected function setUp() {
+		$this->setBrowser ( 'firefox' );
+		$this->setBrowserUrl ( 'https://szkolenia.bananascrum.com' );
 	}
 	
-		/**
-		 * @test
-		 */
-		public function loginTest(){
-			
-			$this->url('/');
-			
-			$this->byId("login")->value('admin');
-			$this->byId('password')->value('password');
-			$this->byClassName('button-small')->click();
-			
-			$this->assertEquals('szkolenia', $this->byClassName('domain-name')->text());
-	
+	/**
+	 * @test
+	 */
+	public function loginTest() {
+		$this->url ( '/' );
+		
+		$this->byId ( "login" )->value ( 'admin' );
+		$this->byId ( 'password' )->value ( 'password' );
+		$this->byClassName ( 'button-small' )->click ();
+		
+		$this->assertEquals ( 'szkolenia', $this->byClassName ( 'domain-name' )->text () );
 	}
 }	
