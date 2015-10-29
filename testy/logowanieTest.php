342bba5707b72906a1a82b45ff32b1bc1255909e
azaczek <azaczek@gbox.pl>
2015-10-29 12:41:26 +0100

    test logowania

---
+++ testy/logowanieTest.php
@@ -0,0 +1,25 @@
+<?php
+class BananaScrumTest extends PHPUnit_Extensions_Selenium2TestCase{
+	
+	protected function setUp()
+	{
+		$this->setBrowser('firefox');
+		$this->setBrowserUrl('https://szkolenia.bananascrum.com');		
+	
+	}
+	
+		/**
+		 * @test
+		 */
+		public function loginTest(){
+			
+			$this->url('/');
+			
+			$this->byId("login")->value('admin');
+			$this->byId('password')->value('password');
+			$this->byClassName('button-small')->click();
+			
+			$this->assertEquals('szkolenia', $this->byClassName('domain-name')->text());
+	
+	}
+}	
