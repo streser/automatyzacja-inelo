711c631e23e2e883d4ec1bb3d1eada738852a687
marek2742 <ziomber.marek@gmail.com>
2015-10-29 12:24:04 +0100

    dodanie pliku BananaLoginTest.php

---
+++ src/BannanaLoginTest.php
@@ -0,0 +1,21 @@
+<?php
+class BannanaScrumTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+	protected function setUp()
+	{
+		$this->setBrowser('firefox');
+		$this->setBrowserUrl('https://szkolenia.bananascrum.com');
+	}
+
+	/**
+	 * @test
+	 */
+	public function shouldLogIn()
+	{
+		$this->url('/');
+		$this->byId('login')->value('admin');
+		$this->byId('password')->value('password');
+		$this->assertEquals('szkolenia', $this->byClassName('domain-name')->text());
+	}
+
+}
