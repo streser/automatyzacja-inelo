3c9f38f1dc4377db21a13f2895df2ac1a6816159
karolinap <karolina.plawecka@gmail.com>
2015-10-29 13:36:46 +0100

    Testy logowania z blednymi danymi

--- src/tests/ValidateLogInTest.php
+++ src/tests/ValidateLogInTest.php
@@ -24,4 +24,36 @@ class ValidateLogInTest extends PHPUnit_Extensions_Selenium2TestCase {
 		
 		$this->assertTrue($result);
 	}
+	
+	/**
+	 * @dataProvider incorrectLoginPasswordProvider
+	 * @test
+	 */
+	public function shouldNotLogIn($login, $pswd) {
+		$this->url('/');
+	
+		$this->byId('login')->value($login);
+		$this->byId('password')->value($pswd);
+	
+		$this->byName('commit')->click();
+	
+		try {
+			$result = $this->byId('forgot-password')->displayed();
+		} catch (Exception $e) {
+			$this->fail('Uda³o siê zalogowaæ');
+		}
+	
+		$this->assertTrue($result);
+	}
+	
+	public function incorrectLoginPasswordProvider() {
+		return array(
+				array('admin', 'admin'),
+				array('password', 'password'),
+				array('test', 'test'),
+				array('', 'password'),
+				array('admin', ''),
+				array('', '')
+		);
+	}
 }
