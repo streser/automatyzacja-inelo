a7903a3f6a734a144550d6cf1087ace80e29c09a
sgolabek <sgolabek@gbox.pl>
2015-10-30 11:07:34 +0100

    test dodajacy i sprawdzajacy czy sie dodal item

--- src/POP/Pages/BeglogPage.php
+++ src/POP/Pages/BeglogPage.php
@@ -7,6 +7,7 @@ class BeglogPage extends Page {
 	 * @var PHPUnit_Extensions_Selenium2TestCase 
 	 */
 	private $driver;
+	private $itemText;
 
 	function __construct($driver){
 			$this->driver = $driver;
@@ -17,14 +18,86 @@ class BeglogPage extends Page {
 		
 		$resoult = $this->driver->byClassName('backlog-header-title')->text();
 		
-		
 		if($resoult == 'Product backlog'){
 			return true;
 		}
 		else{
 			return false;
 		}
+	}
+	
+	private function findById($id){
+		
+		$elem = $this->driver->waitUntil($func = function ($arg) use ($id) {
+			$elem = $this->driver->byId($id);
+			return $elem;
+		}
+		,3000);
+		
+		return $elem;
 		
 	}
+	
+	private function findByXpath($id){
+	
+		$elem = $this->driver->waitUntil($func = function ($arg) use ($id) {
+			$elem = $this->driver->byXPath($id)->text();
+			return $elem;
+		}
+		,3000);
+	
+			return $elem;
+	
+	}
+	
+	
+	public function addItem(){
+		
+		$this->driver->byClassName('new-backlog-item-button')->click();
+			
+		$this->findById('item_user_story')->value('udalo sie');
+		
+		$this->findById('item_description')->value('Description');
+		
+		$this->driver->byXPath("//div[@class='new-item-form-controls']/input")->click();
+		
+		$elem = $this->findByXpath("//div[contains(text(), 'udalo sie')]");
+		
+
+		//$this->driver->assertEquals('udalo sie', $elem);
+
+		
+		$this->itemText = $elem;
+		
+		
+	}
+	
+	  
+	 public function deleteItem() {
+	 
+		$element = $this->driver->waitUntil($func = function ($arg) {
+			$element = $this->driver->byXPath("//div[div[contains(text(), 'udalo sie')]]/div[@class='controls']/img[@class='trash delete-item']");
+			
+			
+			return $element;
+		}
+		,3000);
+		
+		
+		$element->click();
+		$this->driver->acceptAlert();
+		$this->driver->assertEquals('udalo sie', $elem);
+	}
+
+	
+	public function isItemAdded(){
+		if($this->itemText = 'udalo sie'){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
 
 }
--- src/POP/Tests/ItemTest.php
+++ src/POP/Tests/ItemTest.php
@@ -1,33 +1,34 @@
 <?php
-
 include('src/POP/Pages/LoginPage.php');
 //include('src/POP/Pages/BeglogPage.php');
 
 class LoginTest extends PHPUnit_Extensions_Selenium2TestCase {
 
-	
+
 	public function setUp(){
 		$this->setBrowser('firefox');
 		$this->setBrowserUrl('https://szkolenia.bananascrum.com/');
 	}
-	
+
 	/**
 	 * @test
 	 */
-	public function shouldLogIn() {
-	
+	public function shouldAddItem() {
+
 		//given
 		$loginPage = (new LoginPage($this))->open();
-		
+
 		//alternatywnie mozna tak
 		//$loginPage = new LoginPage($this);
 		//$loginPage->open
-		
-		
+
+
 		//when
 		$productBeglogPage = $loginPage->correctLogin();
 		
+		$productBeglogPage->addItem();
+		
 		//then
-		$this->assertTrue($productBeglogPage->isOpen($this));
+		$this->assertTrue($productBeglogPage->isItemAdded());
 	}
 }
